0950e7b80dc04ba72c9bf774f74c02f13b78bd75
Phong <oogiovamayoo2013@gmail.com>
2017-12-22 23:11:59 +0700

    1
    1

---
+++ InnerBean/src/com/java/MainApp.java
@@ -0,0 +1,15 @@
+package com.java;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("Bean.xml");
+		TextEditor te = (TextEditor) context.getBean("textEditor");
+		te.getSpellChecker().checkSpelling();
+		
+//		SpellChecker sp = (SpellChecker) context.getBean("spellChecker");
+	}
+}
---
+++ InnerBean/src/com/java/SpellChecker.java
@@ -0,0 +1,11 @@
+package com.java;
+
+public class SpellChecker {
+	public SpellChecker() {
+		System.out.println("Inside spellchecker contructor...");
+	}
+	
+	public void checkSpelling() {
+		System.out.println("Inside checkSpelling...");		
+	}
+}
