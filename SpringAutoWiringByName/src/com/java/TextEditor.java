cd17bd681299a4cc043fc8ac740233d36bee2f8f
Phong <oogiovamayoo2013@gmail.com>
2017-12-23 01:33:00 +0700

    3
    3

---
+++ SpringAutoWiringByName/src/com/java/TextEditor.java
@@ -0,0 +1,18 @@
+package com.java;
+
+public class TextEditor {
+	private SpellChecker spellChecker;
+	private String name;
+	public SpellChecker getSpellChecker() {
+		return spellChecker;
+	}
+	public void setSpellChecker(SpellChecker spellChecker) {
+		this.spellChecker = spellChecker;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}	
+}
