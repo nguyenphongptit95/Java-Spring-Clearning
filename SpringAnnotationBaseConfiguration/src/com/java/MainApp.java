a1a67751f1c781c371df5f368624b60129f74ff1
Phong <oogiovamayoo2013@gmail.com>
2017-12-23 19:31:48 +0700

    7
    7

---
+++ SpringAnnotationBaseConfiguration/src/com/java/MainApp.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+	public static void main(String[]args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("Bean.xml");
+		Student st = (Student) context.getBean("student");
+		System.out.println(st.getAge());
+		System.out.println(st.getName());
+	}
+}
